72bef4e6b5995021c47dba9ae229f874a42bce6f
lausaru <laurasanchezru@gmail.com>
2024-02-10 17:10:48 +0100

    section 1 done fix

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -22,31 +22,24 @@ class MainTest {
     @DisplayName("Test for n = 5")
     @Test
     void arrayOddInts_Testn5() {
-        Main main = new Main();
-
         assertEquals(Arrays.asList(1, 3), main.buildArrayOddInts(5));
     }
 
     @DisplayName("Test for n = 1")
     @Test
     void arrayOddInts_Testn1() {
-        Main main = new Main();
-
         assertEquals(Arrays.asList(), main.buildArrayOddInts(1));
     }
 
     @DisplayName("Test for n = -1")
     @Test
     void arrayOddInts_Testnegativen() {
-        Main main = new Main();
-
         assertEquals(Arrays.asList(), main.buildArrayOddInts(-1));
     }
 
     @DisplayName("Test for n = 6 (even num)")
     @Test
     void arrayOddInts_TestnEven() {
-        Main main = new Main();
         assertEquals(Arrays.asList(1,3,5), main.buildArrayOddInts(6));
     }
 }
